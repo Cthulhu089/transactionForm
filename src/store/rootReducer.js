cbe11b032e2a15043a30f14bcff79212ab5b17fb
Marco Polo Vazquez Gutierrez <marco.vazquez@agilethought.com>
2020-10-15 18:07:44 -0500

    Remove unused files

--- src/store/rootReducer.js
+++ src/store/rootReducer.js
@@ -1,10 +1,6 @@
 import { combineReducers } from 'redux';
-import pokeReducer from '../reducers/pokeListReducer';
-import pokeInfoReducer from '../reducers/pokeInfoReducer';
 import { routerReducer } from "react-router-redux";
 
 export default combineReducers({
-    pokeReducer,
-    pokeInfoReducer,
     routing: routerReducer,
 });
